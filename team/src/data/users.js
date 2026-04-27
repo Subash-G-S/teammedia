fd9e368b297687883749bd06e2b84d2f78de8124
Subash-G-S <subashgopi2006@gmail.com>
2026-04-27 19:03:04 +0530

    user added

--- team/src/data/users.js
+++ team/src/data/users.js
@@ -1,5 +1,5 @@
 export const usersMap = {
   "subashgopi2006@gmail.com": "Subash",
   "rajdeepkanchana11@gmail.com": "Rajdeep",
-  "yourmail3@gmail.com": "Kiran"
+  "strixsachin@gmail.com": "Sachin"
 }
